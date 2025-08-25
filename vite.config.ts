9b4259aa817020637d96400e21bb9714f6803fa5
mpriessner <martin.priessner@gmail.com>
2025-08-25 07:34:30 +0200

    github-updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/sci-symbio-vision/' : '/',
   server: {
     host: "::",
     port: 8080,
